40b642d455a360f0103ebb9d847761f37182971d
davimart1ns <davisantosmartins144@gmail.com>
2026-03-07 01:48:54 -0300

    melhora a autenticação com JWT Tokens

--- auth_routes.py
+++ auth_routes.py
@@ -1,16 +1,40 @@
 from fastapi import APIRouter, Depends, HTTPException
 from models import Usuario
 from dependencies import pegar_sessao
-from main import bcrypt_context
+from main import bcrypt_context, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY
 from schemas import UsuarioSchema, LoginSchema
 from sqlalchemy.orm import Session
+from jose import jwt, JWTError
+from datetime import datetime, timedelta, timezone
+
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 
-def criar_token(id_usuario):
-    token = "tessdgsdggeythlhb3{id_usuario}" 
-    return token
+def verificar_token(token, session: Session = Depends(pegar_sessao)):
+    # verificar se o usuário é valido
+    # extrair o id do usuário
+    usuario = session.query(Usuario).filter(Usuario.id == 1).first()
+    return usuario
+
+
+def criar_token(
+    id_usuario, duracao_token=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+):
+    data_expiracao = datetime.now(timezone.utc) + duracao_token
+    dic_info = {"sub": id_usuario, "exp": data_expiracao}
+    jwt_codificado = jwt.encode(dic_info, SECRET_KEY, ALGORITHM)
+    return jwt_codificado
+
+
+def autenticar_usuario(email, senha, session):
+    usuario = session.query(Usuario).filter(Usuario.email == email).first()
+    if not usuario:
+        return False
+    elif not bcrypt_context.verify(senha, usuario.senha):
+        return False
+    return usuario
+
 
 @auth_router.get("/")
 async def home():
@@ -24,13 +48,23 @@ async def home():
 
 
 @auth_router.post("/criar_conta")
-async def criar_conta(usuario_schema: UsuarioSchema, session: Session = Depends(pegar_sessao)):
-    usuario = session.query(Usuario).filter(Usuario.email == usuario_schema.email).first()
+async def criar_conta(
+    usuario_schema: UsuarioSchema, session: Session = Depends(pegar_sessao)
+):
+    usuario = (
+        session.query(Usuario).filter(Usuario.email == usuario_schema.email).first()
+    )
     if usuario:
         raise HTTPException(status_code=400, detail="E-mail do usuário já cadastrado")
     else:
         senha_criptografada = bcrypt_context.hash(usuario_schema.senha)
-        novo_usuario = Usuario(usuario_schema.nome, usuario_schema.email, senha_criptografada, usuario_schema.ativo, usuario_schema.admin)
+        novo_usuario = Usuario(
+            usuario_schema.nome,
+            usuario_schema.email,
+            senha_criptografada,
+            usuario_schema.ativo,
+            usuario_schema.admin,
+        )
         session.add(novo_usuario)
         session.commit()
         return {"mensagem": f"Usuário cadastrado com sucesso {usuario_schema.email}"}
@@ -38,12 +72,26 @@ async def criar_conta(usuario_schema: UsuarioSchema, session: Session = Depends(
 
 @auth_router.post("/login")
 async def login(login_schema: LoginSchema, session: Session = Depends(pegar_sessao)):
-    usuario = session.query(Usuario).filter(Usuario.email == login_schema.email).first()
+    usuario = autenticar_usuario(login_schema.email, login_schema.senha, session)
     if not usuario:
-        raise HTTPException(status_code=400, detail="Usuário não encontrado!")
+        raise HTTPException(
+            status_code=400, detail="Usuário não encontrado ou crendenciais inválidas!"
+        )
     else:
         access_token = criar_token(usuario.id)
+        refresh_token = criar_token(usuario.id, duracao_token=timedelta(days=7))
         return {
             "access_token": access_token,
-            "token_type": "Bearer"
-            }
+            "refresh_token": refresh_token,
+            "token_type": "Bearer",
+        }
+
+
+@auth_router.get("/refresh")
+async def use_refresh_token(token):
+    usuario = verificar_token(token)
+    access_token = criar_token(usuario.id)
+    return {
+        "access_token": access_token,
+        "token_type": "Bearer",
+    }
